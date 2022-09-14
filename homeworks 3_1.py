b29d0d0a460632569368d3ccd1d1995fedd8b107
Slizh <as_bank@mail.ru>
2022-09-14 22:10:32 +0300

    reading 1 test

--- homeworks 3_1.py
+++ homeworks 3_1.py
@@ -4,15 +4,12 @@ text = "there should be a normal text here"
 text = text.replace(' ', '-')
 print(text)
 # Способ второй
-a = 'there should be a normal text here'
-def astra(mstr):
-    print(mstr)
-    res = a.sub(r'\s+', '-', mstr)
-    print(res.split())
-if __name__ == '__sff__':
-    mstr = input()
-    astra(mstr)
-print(b)
+
 # Ввести 3 числа, найти среднее арифмитическое с точностью до 3
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+c = int(input("Enter a number:"))
+print((a+b+c)/3)
 # Ввести Имя, Возраст и Город, сформировать приветственное сообщение путем формирования 3-мя способами
+
 # Ввести 3 числа, сказать сколько из них положительных и сколько отрицательных
